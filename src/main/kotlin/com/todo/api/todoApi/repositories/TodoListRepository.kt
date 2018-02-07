1faa15201817e39dcbe17554d980f39a689df426
samuraiseoul <scottlavigne28@gmail.com>
2018-02-06 20:11:50 -0600

    make repo a crud repo again.

--- src/main/kotlin/com/todo/api/todoApi/repositories/TodoListRepository.kt
+++ src/main/kotlin/com/todo/api/todoApi/repositories/TodoListRepository.kt
@@ -1,8 +1,8 @@
 package com.todo.api.todoApi.repositories
 
 import com.todo.api.todoApi.entities.TodoList
-import org.springframework.data.repository.Repository
+import org.springframework.data.repository.CrudRepository
 
-interface TodoListRepository : Repository<TodoList, Long> {
+interface TodoListRepository : CrudRepository<TodoList, Long> {
     fun findByTodoListId(id: Long?) : List<TodoList>
 }
