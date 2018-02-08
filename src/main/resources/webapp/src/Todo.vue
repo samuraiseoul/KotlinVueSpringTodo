68e15186b1becbaa46474313fc16ba3b5c21db32
samuraiseoul <scottlavigne28@gmail.com>
2018-02-07 23:08:10 -0600

    fix frontend to work with api, recursive lists.

--- src/main/resources/webapp/src/Todo.vue
+++ src/main/resources/webapp/src/Todo.vue
@@ -1,17 +1,43 @@
 <template>
     <li :parentId="list.todoListId">
-        {{list.title}} <button type="button" v-on:click="$emit('delete')">X</button>
+        {{list.title}}
+        <button type="button" v-on:click="$emit('delete')">X</button>
+        <button type="button" v-on:click="add">+</button>
+        <form class="addListForm" @submit.prevent="addList"  v-bind:class="{hide: !isAdding}">
+            <label>Add Entry: <input type="text"/></label>
+            <button>Add List</button> <button type="button" v-on:click="cancelAdd">Cancel</button>
+        </form>
+        <ol>
+            <Todo v-for="(list, index) in list.todoListItems" v-bind:list="list" v-on:delete="deleteList(index)"></Todo>
+        </ol>
     </li>
 </template>
 
 <script>
+    import TodoList from './TodoList';
     export default {
-        props: ['list']
+        props: ['list'],
+        name: 'Todo',
+        data: () => {return {isAdding: false}},
+        methods: {
+            add(){this.isAdding = true;},
+            cancelAdd(){this.isAdding = false;},
+            addList(event){
+                let form = event.target;
+                let input = form.querySelector('input');
+                this.list.todoListItems.push(new TodoList(0, input.value, 0, 0));
+                input.value = '';
+                this.isAdding = false;
+            },
+            deleteList(index){
+                this.list.todoListItems.splice(index, 1);
+            }
+        }
     }
 </script>
 
 <style scoped>
-    li {
-        color: red;
+    .hide{
+        display: none;
     }
 </style>
--- src/main/resources/webapp/src/TodoListApp.vue
+++ src/main/resources/webapp/src/TodoListApp.vue
@@ -39,5 +39,4 @@
 </script>
 
 <style scoped>
-    h3 {color: blue;}
 </style>
